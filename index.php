1eb687d736ab432a31add74ea497d724f3349dcc
yahyaubaid <muhammad.ubaid@students.amikom.ac.id>
2020-12-14 22:00:27 +0700

    footer go-rent

--- index.php
+++ index.php
@@ -1142,7 +1142,7 @@
                 <div class="row">
                     <div class="col-lg-12 text-center">
                         <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> Go-Rent
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
                     </div>
                 </div>
