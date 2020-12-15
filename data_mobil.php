384eb8fed678bec5f5c06ec0b7658543cb0da674
Rmuflih <mufliharifinrahmat@gmail.com>
2020-12-15 21:24:10 +0700

    pindah footer ke footer.php dan header ke header.php sama nambah data_mobil.php

---
+++ data_mobil.php
@@ -0,0 +1,86 @@
+    <?php include('./inc/customer/header.php');?>
+    <!--== Page Title Area Start ==-->
+    <section id="page-title-area" class="section-padding overlay">
+        <div class="container">
+        
+            <div class="row">
+                <!-- Page Title Start -->
+                <div class="col-lg-12">
+                    <div class="section-title  text-center">
+                        <h2>Mobil Kami</h2>
+                        <span class="title-line"><i class="fa fa-car"></i></span>
+                        <p>Menampilkan hasil dari mobil yang siap dirental</p>
+                    </div>
+                </div>
+                <!-- Page Title End -->
+            </div>
+        </div>
+    </section>
+    <!--== Page Title Area End ==-->
+
+    <!--== Car List Area Start ==-->
+    <section id="car-list-area" class="section-padding">
+        <div class="container">
+            <div class="row">
+                <!-- Car List Content Start -->
+                <div class="col-lg-12">
+                    <div class="car-list-content">
+                        <div class="row">
+                            <!-- Single Car Start -->
+                            <div class="col-lg-6 col-md-6">
+                                <div class="single-car-wrap">
+                                    <img class="car-list-thumb" src="" alt="">
+                                    <div class="car-list-info without-bar">
+                                        <h2></h2>
+                                        <h5>Rp.  /hari</h5>
+                                        <ul class="car-info-list">
+                                            <li> AC
+                                            </li>
+                                            <li> Supir
+                                            </li>
+                                            <li> Diesel
+                                            </li>
+                                            <li> Auto
+                                            </li>
+                                            
+                                        </ul>
+                                        <p class="rating">
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star unmark"></i>
+                                        </p>
+                                        
+                                        <a href="" class="rent-btn">Detail</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Single Car End -->
+
+                            
+                        </div>
+                    </div>
+
+                    <!-- Page Pagination Start -->
+                    <div class="page-pagi">
+                        <nav aria-label="Page navigation example">
+                            <ul class="pagination">
+                                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                                <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item"><a class="page-link" href="#">4</a></li>
+                                <li class="page-item"><a class="page-link" href="#">5</a></li>
+                                <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <!-- Page Pagination End -->
+                </div>
+                <!-- Car List Content End -->
+            </div>
+        </div>
+    </section>
+    <!--== Car List Area End ==-->
+    <?php include('./inc/customer/footer.php');?>
---
+++ inc/customer/footer.php
@@ -0,0 +1,144 @@
+    <!--== Footer Area Start ==-->
+    <section id="footer-area">
+        <!-- Footer Widget Start -->
+        <div class="footer-widget-area">
+            <div class="container">
+                <div class="row">
+                    <!-- Single Footer Widget Start -->
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-footer-widget">
+                            <h2>Tentang Kami</h2>
+                            <div class="widget-body">
+                                <!--<img src="./assets/shop/img/logo.png" alt="JSOFT">-->
+                                <p style='font-size:40px;margin-top:-15px;'>GO! Rent</p>
+                                <p>Lorem ipsum dolored is a sit ameted consectetur adipisicing elit. Nobis magni assumenda distinctio debitis, eum fuga fugiat error reiciendis.</p>
+
+                                <div class="newsletter-area">
+                                    <form action="index.html">
+                                        <input type="email" placeholder="Subscribe Our Newsletter">
+                                        <button type="submit" class="newsletter-btn"><i class="fa fa-send"></i></button>
+                                    </form>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Single Footer Widget End -->
+
+                    <!-- Single Footer Widget Start -->
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-footer-widget">
+                            <h2>Postingan Sebelumnya</h2>
+                            <div class="widget-body">
+                                <ul class="recent-post">
+                                    <li>
+                                        <a href="#">
+                                           Hello Indonesia! 
+                                           <i class="fa fa-long-arrow-right"></i>
+                                       </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                          Lorem ipsum dolor sit amet
+                                           <i class="fa fa-long-arrow-right"></i>
+                                       </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                           Hello Indonesia! 
+                                           <i class="fa fa-long-arrow-right"></i>
+                                       </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            consectetur adipisicing elit?
+                                           <i class="fa fa-long-arrow-right"></i>
+                                       </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Single Footer Widget End -->
+
+                    <!-- Single Footer Widget Start -->
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-footer-widget">
+                            <h2>Lebih Dekat</h2>
+                            <div class="widget-body">
+                                <p>Lorem ipsum doloer sited amet, consectetur adipisicing elit. nibh auguea, scelerisque sed</p>
+
+                                <ul class="get-touch">
+                                    <li><i class="fa fa-map-marker"></i> Bintaro, Jakarta Selatan</li>
+                                    <li><i class="fa fa-mobile"></i> +62 820 7892 6578</li>
+                                    <li><i class="fa fa-envelope"></i> cs@gorent.com</li>
+                                </ul>
+                                <a href="https://goo.gl/maps/b5mt45MCaPB2" class="map-show" target="_blank">Lihat Lokasi</a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Single Footer Widget End -->
+                </div>
+            </div>
+        </div>
+        <!-- Footer Widget End -->
+        
+
+        <!-- Footer Bottom Start -->
+        <div class="footer-bottom-area bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <p>
+                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                                Copyright &copy;<script>document.write(new Date().getFullYear());</script> Go! Rent
+                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer Bottom End -->
+    </section>
+    <!--== Footer Area End ==-->
+
+    <!--== Scroll Top Area Start ==-->
+    <div class="scroll-top">
+        <img src="./assets/shop/img/scroll-top-blue.png" alt="JSOFT">
+    </div>
+    <!--== Scroll Top Area End ==-->
+
+    <!--=======================Javascript============================-->
+    <!--=== Jquery Min Js ===-->
+    <script src="./assets/shop/js/jquery-3.2.1.min.js"></script>
+    <!--=== Jquery Migrate Min Js ===-->
+    <script src="./assets/shop/js/jquery-migrate.min.js"></script>
+    <!--=== Popper Min Js ===-->
+    <script src="./assets/shop/js/popper.min.js"></script>
+    <!--=== Bootstrap Min Js ===-->
+    <script src="./assets/shop/js/bootstrap.min.js"></script>
+    <!--=== Gijgo Min Js ===-->
+    <script src="./assets/shop/js/plugins/gijgo.js"></script>
+    <!--=== Vegas Min Js ===-->
+    <script src="./assets/shop/js/plugins/vegas.min.js"></script>
+    <!--=== Isotope Min Js ===-->
+    <script src="./assets/shop/js/plugins/isotope.min.js"></script>
+    <!--=== Owl Caousel Min Js ===-->
+    <script src="./assets/shop/js/plugins/owl.carousel.min.js"></script>
+    <!--=== Waypoint Min Js ===-->
+    <script src="./assets/shop/js/plugins/waypoints.min.js"></script>
+    <!--=== CounTotop Min Js ===-->
+    <script src="./assets/shop/js/plugins/counterup.min.js"></script>
+    <!--=== YtPlayer Min Js ===-->
+    <script src="./assets/shop/js/plugins/mb.YTPlayer.js"></script>
+    <!--=== Magnific Popup Min Js ===-->
+    <script src="./assets/shop/js/plugins/magnific-popup.min.js"></script>
+    <!--=== Slicknav Min Js ===-->
+    <script src="./assets/shop/js/plugins/slicknav.min.js"></script>
+
+    <!--=== Mian Js ===-->
+    <script src="./assets/shop/js/main.js"></script>
+
+</body>
+
+</html>
