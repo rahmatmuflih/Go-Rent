f254350b7862c8401252e9c8ccf55f74ea25768b
Rmuflih <mufliharifinrahmat@gmail.com>
2020-12-22 22:06:58 +0700

    update logo

--- admin/index.php
+++ admin/index.php
@@ -44,7 +44,7 @@ if(isset($_POST['login'])){
         <div class="row">
           <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
             <div class="login-brand logo">
-              <h1 class="tulisan-logo">Logo</h1>
+              <img src="../assets/img/logo.png" alt="" style="width:150px;height:50px;">
               <!--<img src="" alt="logo" width="100" class="shadow-light rounded-circle" class='image'>-->
             </div>
 
